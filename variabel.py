797864cb2d569d1e7a2a88b9b4a0b90ac1f25426
mstofa26 <86153222+mstofa26@users.noreply.github.com>
2021-06-19 18:53:46 +0700

    Add files via upload

---
+++ variabel.py
@@ -0,0 +1,9 @@
+print("Hello World!!!")
+masukan = str(input("Masukkan Nama Perusahaan: "))
+nama = "compnet"
+out = "Nama Perusahaan Kita"
+out2 = "Bukan Nama Perusahaan Kita"
+if masukan == nama:
+    print(out)
+else:
+    print(out2)
